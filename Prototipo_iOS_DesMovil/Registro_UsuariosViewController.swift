80a36f39d93b166ff5b6c6e796d8353f3b73ff5b
MacBookPro <APPLE@MacBook-Pro-de-abraham.local>
2017-01-16 11:43:35 -0600

    Restablecido el google info.plist

---
+++ Prototipo_iOS_DesMovil/Registro_UsuariosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Registro_UsuariosViewController.swift
+//  Prototipo_iOS_DesMovil
+//
+//  Created by MacBookPro on 16/01/17.
+//  Copyright © 2017 Integra IT. All rights reserved.
+//
+
+import UIKit
+
+class Registro_UsuariosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
